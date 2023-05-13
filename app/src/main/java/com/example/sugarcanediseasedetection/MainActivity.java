8d4a0c2669542124200e93c69f04dea35ef25265
Riya Bhandari <bhandari28riya@gmail.com>
2023-05-13 10:01:35 +0530

    :bug: Got ro.mediatek.platform bug while integrating the model file to app

--- app/src/main/java/com/example/sugarcanediseasedetection/MainActivity.java
+++ app/src/main/java/com/example/sugarcanediseasedetection/MainActivity.java
@@ -11,20 +11,36 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.sugarcanediseasedetection.ml.Model;
 import com.github.dhaval2404.imagepicker.ImagePicker;
 
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     static final String USER_IMG_KEY = "UserImgKey";
-    static final int REQ_CODE = 100 ;
+    static final int REQ_CODE = 100;
+    Bitmap bitmap;
+    TextView result;
+
+    int imageSize = 512;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button changelng = findViewById(R.id.changeMyLang);
+        result = (TextView) findViewById(R.id.resultt);
         changelng.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,28 +56,25 @@ public class MainActivity extends AppCompatActivity {
                 showChangeLanguageDialog();
             }
         });
-        
+
     }
 
     private void showChangeLanguageDialog() {
-        final String[] listItems = {"हिंदी" , "मराठी", "English"}; //array of languages
+        final String[] listItems = {"हिंदी", "मराठी", "English"}; //array of languages
         AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
         mBuilder.setTitle("Choose Language...");
         mBuilder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                if (i == 0)
-                {
+                if (i == 0) {
                     setLocale("hi");
                     recreate();
                 }
-                if (i == 1)
-                {
+                if (i == 1) {
                     setLocale("mr");
                     recreate();
                 }
-                if (i == 2)
-                {
+                if (i == 2) {
                     setLocale("en");
                     recreate();
                 }
@@ -83,7 +97,8 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("My_Lang", lang);
         editor.apply();
     }
-//load lang saved in shared p
+
+    //load lang saved in shared p
     public void loadLocale() {
         SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
         String language = prefs.getString("My_Lang", "");
@@ -93,9 +108,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void addImg(View view) {
         ImagePicker.with(MainActivity.this)
-                   .crop()                    //Crop image(Optional), Check Customization for more option
-                   .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
-                   .start();
+                .crop()                    //Crop image(Optional), Check Customization for more option
+                .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
+                .start();
     }
 
     // Data => User data
@@ -105,16 +120,76 @@ public class MainActivity extends AppCompatActivity {
         Uri uri = data.getData(); // User selected image will be in data and it will return the
         // data in URI format
 
+        // URI to bitmap
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         if (resultCode == Activity.RESULT_OK) {
             //Image Uri will not be null for RESULT_OK
             // Passing Data
-            Intent iPassData = new Intent(MainActivity.this , ResultActivity.class);
-            iPassData.putExtra(USER_IMG_KEY , uri);
-            startActivity(iPassData);
+//            Intent iPassData = new Intent(MainActivity.this , ResultActivity.class);
+//            iPassData.putExtra(USER_IMG_KEY , uri);
+//            startActivity(iPassData);
+            // Resizing our bitmap
+            bitmap = Bitmap.createScaledBitmap(bitmap, imageSize, imageSize, false);
+            modelFunction(bitmap);
         } else if (resultCode == ImagePicker.RESULT_ERROR) {
             Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, "Pls Select Image", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void modelFunction(Bitmap bitmap) {
+        try {
+            Model model = Model.newInstance(MainActivity.this);
+
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 512, 512, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+            int[] intValues = new int[imageSize * imageSize];
+            bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+            int pixels = 0;
+            // Iterate over each pixel and extract RGB values . add those values individually to byte buffer .
+            for (int i = 0; i < imageSize; i++) {
+                for (int j = 0; j < imageSize; j++) {
+                    int val = intValues[pixels++]; // RGB Values
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 1));
+                }
+            }
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Our result are stored in outputFeature0
+            Model.Outputs outputs = model.process(inputFeature0);
+            // Runs model inference and gets result.
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+           float[] confidences = outputFeature0.getFloatArray();
+           // Find the index of the class with the biggest confidence .
+            int maxPos = 0;
+            float maxConfidence = 0 ;
+            for (int i = 0; i < confidences.length; i++) {
+                if (confidences[i] > maxConfidence){
+                    maxConfidence = confidences[i];
+                    maxPos = i ;
+                }
+            }
+
+            // Labels that our model is trained for
+            String[] labels = {"Red Strip" , "Leaf Scald" , "Rust"};
+            result.setText(labels[maxPos]);
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+    }
+
 }
